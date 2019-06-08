dcc222833d0bffe23a52eaab91b24fcd16c696d0
Igor Fabiano Nazar <ifnazar@gmail.com>
2019-06-08 12:40:25 -0300

    Error stack trace

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+class CA {
+  async method1() {
+    throw new Error('FOO');
+  }
+}
+
+class C2 {
+  async method2() {
+    await new CA().method1();
+  }
+}
+
+class C3 {
+  async method3() {
+    await new C2().method2();
+  }
+}
+
 async function main() {
-  // eslint-disable-next-line no-console
-  console.log('Hello');
+  await new C3().method3();
 }
 
 main().catch((error) => {
