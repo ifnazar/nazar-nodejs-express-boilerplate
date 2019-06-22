a7f0ce5bc6a91a5852ce5e9233b99f630f3ba28e
Igor Fabiano Nazar <ifnazar@gmail.com>
2019-06-22 11:33:33 -0300

    Using Helmet to improve security

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
 import statusHandler from './routes/status-handler';
+
+/**
+ * @param {Express} app
+ */
+function configureSecurity(app) {
+  app.use(helmet());
+  app.use(helmet.noCache());
+}
 
 // Configure express
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+configureSecurity(app);
 
 // Routes
 app.get('/', statusHandler);
