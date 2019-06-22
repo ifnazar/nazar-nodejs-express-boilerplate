e6f9d8e183568f16ede2d14de3f10fcee9de5d87
Igor Fabiano Nazar <ifnazar@gmail.com>
2019-06-22 11:12:33 -0300

    Add logger

---
+++ src/utils/logger.js
@@ -0,0 +1,24 @@
+/**
+ */
+class Logger {
+  info(message) {
+    const timestamp = new Date().toISOString();
+    // eslint-disable-next-line no-console
+    console.log(`${timestamp} - ${message}`);
+  }
+
+  error(message) {
+    const timestamp = new Date().toISOString();
+    // eslint-disable-next-line no-console
+    console.error(`${timestamp} - ${message}`);
+  }
+
+  warn(message) {
+    const timestamp = new Date().toISOString();
+    // eslint-disable-next-line no-console
+    console.warn(`${timestamp} - ${message}`);
+  }
+}
+const logger = new Logger();
+
+export { Logger, logger };
